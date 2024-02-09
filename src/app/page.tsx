db79e9b77e91aa9903c70a5d5c57ca2a0f01100b
Elina Gorshkova <52108095+elinagorshkova@users.noreply.github.com>
2024-02-09 08:21:34 -0800

    Add UI (#1)
    Co-authored-by: Elina Gorshkova <egorshkova@attentivemobile.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@ import Chat from "./chat/page";
 
 export default function Home() {
   return (
-    <div className="page-container">
-      <h1>Attentive Company Kick-off 2024 THE RPG</h1>
-      <p>You are a slightly drunk Attentive employee at your company kick off in 2024 and you&apos;re down 200 bucks, you&apos;re slightly tired, and some of your colleagues still respect you.</p>
-      <p>Check out is in 10 hours, and you&apos;re not sure how you want to spend your time yet. You enter the casino and you see a very loud and happy craps table. You think Todd is playing.</p>
-      <p>Meanwhile Bentley is losing money at Blackjack while Jesse seems entertained.</p>
-        <Chat />
+    <div className="wrapper">
+      <div className="page-container">
+        <h1 className="title">Attentive Company Kick-off 2024 THE RPG</h1>
+        <div className="text-box">
+          <p>You are a slightly drunk Attentive employee at your company kick off in 2024 and you&apos;re down 200 bucks, you&apos;re slightly tired, and some of your colleagues still respect you.</p>
+          <p>Check out is in 10 hours, and you&apos;re not sure how you want to spend your time yet. You enter the casino and you see a very loud and happy craps table. You think Todd is playing.</p>
+          <p>Meanwhile Bentley is losing money at Blackjack while Jesse seems entertained.</p>
+        </div>
+          <Chat />
+      </div>
     </div>
   );
 }
