07142ce73f7d8826b0c3d1af463c0eaa2eafaffb
Alberto Pareja-Lecaros <aparejalecaros@attentivemobile.com>
2024-02-09 14:00:20 -0600

    Add scrolling, remove attributes from prompt, add spacing (#2)

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -2,19 +2,28 @@
 
 import { Message } from 'ai';
 import { useChat } from 'ai/react';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 export default function Chat() {
     const [money, setMoney] = useState(200);
     const [endurance, setEndurance] = useState(50);
     const [dignity, setDignity] = useState(50);
+    const ref = useRef<HTMLDivElement>(null);
+
+    function scrollDown() {
+        ref.current?.scrollIntoView({
+            behavior: "smooth",
+            block: "end",
+          });   
+    }
+
     function updateStats(message: Message) {
         if (message) {
             const regex = /\{[^{}]*\}/;
             const match = message.content.match(regex);
             if (match) {
                 try {
-                    const formattedString = match[0].replace(/([\{\s,])(\w+)(?=:)/g, '$1"$2"').replace('+', '');
+                    const formattedString = match[0].replace(/([\{\s,])(\w+)(?=:)/g, '$1"$2"').replaceAll('+', '');
                     const statsObj: {money: number; endurance: number; dignity: number; } = JSON.parse(formattedString);
                     console.log(statsObj)
                     setMoney(money + statsObj.money);
@@ -26,8 +35,16 @@ export default function Chat() {
             }
         }
     }
-    const { messages, input, handleInputChange, handleSubmit } = useChat({onFinish: (message) => {updateStats(message)}});
-
+    const { messages, input, handleInputChange, handleSubmit } = useChat({onFinish: (message) => {
+        updateStats(message);
+        scrollDown();
+    }});
+   
+    useEffect(() => {
+        if (messages.length) {
+            scrollDown();
+        }
+      }, [messages.length]);
 
   return (
     <>
@@ -39,9 +56,10 @@ export default function Chat() {
     </div>
     <div className='chat-container'>
         <div className='chat'>
-            {messages.map(m => (
-                <div key={m.id}>
-                    <strong>{m.role}:</strong> {m.content}
+            {messages.map(m => { m.content = m.content.replace(/\{[^{}]*\}/g, ''); return m; }).map((m, index) => (
+                <div key={m.id} className="paragraph">
+                    <strong>{m.role === "user" ? "You" : "Narrator"}:</strong> {m.content}
+                    {index === messages.length - 1 && <div ref={ref} />}
                 </div>
             ))}
 
@@ -49,7 +67,7 @@ export default function Chat() {
                 <input
                 className='chat-input-field'
                     value={input}
-                    placeholder="Say something..."
+                    placeholder="What do you want to do next?"
                     onChange={handleInputChange}
                 />
             </form>
