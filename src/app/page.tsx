ca72167aa46e17d0ea044ec3c81188e1837b13e2
Elina Gorshkova <egorshkova@attentivemobile.com>
2024-02-09 15:57:17 -0500

    Remove real people from the story

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,7 @@ export default function Home() {
         <h1 className="title">Attentive Company Kick-off 2024 THE RPG</h1>
         <div className="text-box">
           <p>You are a slightly drunk Attentive employee at your company kick off in 2024 and you&apos;re down 200 bucks, you&apos;re slightly tired, and some of your colleagues still respect you.</p>
-          <p>Check out is in 10 hours, and you&apos;re not sure how you want to spend your time yet. You enter the casino and you see a very loud and happy craps table. You think Todd is playing.</p>
-          <p>Meanwhile Bentley is losing money at Blackjack while Jesse seems entertained.</p>
+          <p>Check out is in 10 hours, and you&apos;re not sure how you want to spend your time yet. You enter the casino and you see a very loud and happy craps table.</p>
         </div>
           <Chat />
       </div>
